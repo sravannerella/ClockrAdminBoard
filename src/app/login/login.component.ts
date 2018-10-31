5606d5aed8f68010cbed2a720fa54a01a0dfe4a6
Sravan Nerella <sravannerella007@hotmail.com>
2018-10-31 00:48:50 -0400

    form updated and working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { validateConfig } from '@angular/router/src/config';
 
 @Component({
 	selector: 'app-login',
@@ -9,16 +10,57 @@ import { FormControl, Validators } from '@angular/forms';
 
 export class LoginComponent implements OnInit {
 
-	isValidEmail = false;
+	loginForm;
+	iteration = 0;
+	submitForm = false;
+	questions = [
+		{type: 'email', label: 'Email Or Phone', placeholder: 'Enter Email or Phone', controlName: 'email', errMsg: "Couldn't find your account"},
+		{type: 'password', label: 'Password', placeholder: 'Enter Password', controlName: 'pass', errMsg: "Invalid password"}
+	]
 
-	constructor() {
+	constructor(private fb: FormBuilder) {
+		this.loginForm = this.fb.group({
+			email: ['', [
+				Validators.required,
+				Validators.pattern(/^(\d{10})|\b[\w|\d]+@[\w|\d]+\.\w{2,}/)
+			]],
+			pass: ['', [
+				Validators.required,
+				Validators.minLength(5)
+			]]
+		});
 	}
 
 	ngOnInit() {
 	}
 
-	login(value) {
-		console.log(value);
+	login() {
+		console.log(this.loginForm.value);
+	}
+
+	get email() {
+		return this.loginForm.get('email');
+	}
+
+	get password(){
+		return this.loginForm.get('pass');
+	}
+
+	hasError(controlName){
+		let value = this.loginForm.get(controlName);
+		return value.invalid && value.touched;
+	}
+
+	isValid(controlName){
+		let value = this.loginForm.get(controlName);
+		return value.valid;
+	}
+
+	nextQuestion(){
+		this.iteration++;
+		if(this.iteration === (this.questions.length - 1) ){
+			this.submitForm = true;
+		}
 	}
 
 }
